80770c1e22087886fdee6f4ecbffdab3a2c7d7f8
wray-lee <wray_lee@outlook.com>
2024-04-13 03:45:39 +0800

    pref: Prettify codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,43 @@
-import logo from './logo.png';
-import './App.css';
-import {Component, useEffect} from "react";
+import logo from "./logo.png";
+import "./App.css";
+import { Component, useEffect } from "react";
 
-class App extends Component{
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = { apiResponse: "" };
   }
 
   resetHtmlFontSize = () => {
-    document.documentElement.style.fontSize = window.innerWidth / 10 + 'px';
-  }
+    document.documentElement.style.fontSize = window.innerWidth / 10 + "px";
+  };
 
   componentDidMount() {
     this.resetHtmlFontSize();
-    window.addEventListener('resize', this.resetHtmlFontSize);
+    window.addEventListener("resize", this.resetHtmlFontSize);
   }
 
   callAPI() {
     fetch("http://localhost:9000/testAPI")
-        .then(res => res.text())
-        .then(res => this.setState({ apiResponse: res }));
+      .then((res) => res.text())
+      .then((res) => this.setState({ apiResponse: res }));
   }
 
   componentWillMount() {
     this.callAPI();
   }
 
-
   render() {
     return (
       <>
-      <div className="App-header">
-      <p className='apiTest'>{this.state.apiResponse}</p>
-      </div>
-      <div className='App'>
-      <div className='App-logo-move'><img src={logo} className="App-logo" alt="logo" /></div>
-      </div>
+        <div className="App-header">
+          <p className="apiTest">{this.state.apiResponse}</p>
+        </div>
+        <div className="App">
+          <div className="App-logo-move">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </div>
       </>
     );
   }
